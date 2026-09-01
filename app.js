6647f108721d0d0e176a70fe426b3ee713f24a16
rshddgdy-alt <rshddgdy@gmail.com>
2026-09-01 10:47:19 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,41 @@
+async function login(){
+
+const server = document.getElementById("server").value;
+const username = document.getElementById("username").value;
+const password = document.getElementById("password").value;
+
+const btn = document.querySelector("button");
+btn.innerHTML = "جاري الاتصال...";
+
+try {
+
+const api = `/.netlify/functions/proxy?server=${encodeURIComponent(server)}&user=${encodeURIComponent(username)}&pass=${encodeURIComponent(password)}`;
+
+const response = await fetch(api);
+
+const data = await response.json();
+
+if(data.user_info){
+
+alert("تم تسجيل الدخول بنجاح ✅");
+
+localStorage.setItem(
+"ayan_user",
+JSON.stringify(data)
+);
+
+}else{
+
+alert("بيانات الدخول غير صحيحة ❌");
+
+}
+
+}catch(error){
+
+alert("فشل الاتصال بالسيرفر ❌");
+
+}
+
+btn.innerHTML = "تسجيل الدخول";
+
+}
